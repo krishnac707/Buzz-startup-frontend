00154a808a272835217e09928fd95b4b2f5271e7
Renjith <renjith.gencue@outlook.com>
2023-09-25 02:56:54 -0700

    nav bar fix

--- buzz-startup/src/components/header/Header.jsx
+++ buzz-startup/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 import headerImage from './../../images/startup-dashboard-profile-logo.png'
 
-const Header = () => {
+function Header() {
     const router = useNavigate();
 
     const location = useLocation();
@@ -28,7 +28,12 @@ const Header = () => {
     //    setIsOpen(false)
     //    router("/startup-page")
     // }
+    const [expanded, setExpanded] = useState(false);
 
+    const handleNavItemClick = (route) => {
+        router(route) 
+      setExpanded(false); // Close the Navbar when a menu item is clicked
+    };
 
 
 
@@ -88,30 +93,30 @@ const Header = () => {
 
     return (
         <div id='navbar-header' >
-            <Navbar expand="lg" fixed="top" className="bg-body-tertiary header-navbar-main-padding">
+            <Navbar expanded={expanded} expand="lg" fixed="top" className="bg-body-tertiary header-navbar-main-padding">
                 <Container>
                     <div className='navbar-heading-logo-color' ><p className='mb-0 logo-cursor' onClick={() => router("/")}>BuzzStartups</p></div>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
                     <Navbar.Collapse id="basic-navbar-nav" className='navbar-menu-main-div'>
                         <Nav className="me-auto navbar-menu-div" >
-                            <Nav.Link className='navbar-link-menu-div' onClick={() => router("/startup-page")}>Startups</Nav.Link>
-                            <Nav.Link className='navbar-link-menu-div' onClick={() => router("/investor-page")}>Investors</Nav.Link>
-                            <Nav.Link className='navbar-link-menu-div' onClick={() => router("/about-us")}>About Us</Nav.Link>
-                            <Nav.Link className='navbar-link-menu-div' onClick={() => router('/contact-us')}>Contact Us</Nav.Link>
+                            <Nav.Link className='navbar-link-menu-div' onClick={() => { handleNavItemClick("/startup-page") }}>Startups</Nav.Link>
+                            <Nav.Link className='navbar-link-menu-div' onClick={() => { handleNavItemClick("/investor-page") }}>Investors</Nav.Link>
+                            <Nav.Link className='navbar-link-menu-div' onClick={() => { handleNavItemClick("/about-us") }}>About Us</Nav.Link>
+                            <Nav.Link className='navbar-link-menu-div' onClick={() => { handleNavItemClick("/contact-us") }}>Contact Us</Nav.Link>
                             {/* <Nav.Link className='navbar-link-menu-div'>Explore</Nav.Link> */}
                             <NavDropdown title="Explore" className='navbar-link-menu-div' id="basic-nav-dropdown">
-                                <NavDropdown.Item onClick={() => router("/my-innovation")}>MyInnovation</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => router("/pitch-deck")}>
+                                <NavDropdown.Item onClick={() => handleNavItemClick("/my-innovation")}>MyInnovation</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => handleNavItemClick("/pitch-deck")}>
                                     Pitch Deck
                                 </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => router('/financial-modelling')}>Financial Modelling</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => handleNavItemClick("/financial-modelling")}>Financial Modelling</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
-                    
+
                     <Navbar.Collapse id="basic-navbar-nav" className='navbar-menu-main-div-last'>
                         <Nav className="me-auto navbar-menu-div">
-                            <Nav.Link className='navbar-link-menu-div-last' onClick={() => router("/login")}>Login</Nav.Link>
+                            <Nav.Link className='navbar-link-menu-div-last' onClick={() => handleNavItemClick("/login")}>Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
