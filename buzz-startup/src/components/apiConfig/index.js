e0675e45752f139fbd984d55a9406d20a8f5a287
krishna chaudhary <krishnacchaudhary707@gmail.com>
2023-10-19 15:34:45 +0530

    updated link

--- buzz-startup/src/components/apiConfig/index.js
+++ buzz-startup/src/components/apiConfig/index.js
@@ -3,8 +3,8 @@ import axios from "axios"
 const token = JSON.parse(localStorage.getItem("token"))
 
 const api = axios.create({
-    // baseURL:'https://buzz-startups-backend.vercel.app/api/v1',
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL:'https://buzz-startups-backend.vercel.app/api/v1',
+    // baseURL: "http://localhost:8000/api/v1",
     headers: { Authorization: `Bearer ${token}` },
   });
 
